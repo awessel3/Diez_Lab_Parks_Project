8b3dd2544a6bcda384bd67ae7a324b588cde7342
Ava Wessel <149992130+awessel3@users.noreply.github.com>
2025-08-13 15:41:00 -0700

    new files and code 2024

---
+++ Data/2024/24_pheno_exploratory.R
@@ -0,0 +1,188 @@
+library(lubridate)
+library(tidyverse)
+library(neonUtilities)
+library(patchwork)
+library(sp)
+library(raster)
+library(ggplot2)
+library(ggeffects)
+
+pheno_24_alone <- read_rds("pheno_24_alone.rds")
+pheno_24_diverse <- read_rds("pheno_24_diverse.rds")
+
+colnames(pheno_24_alone)
+
+
+park_colors <- c(
+  "BR"  = "#955F8E",  # red
+  "RF"  = "#0F9554",  # green
+  "WIR" = "#17BEBB",  # blue
+  "SEM" = "#F5BB00"   # yellow
+)
+
+# alone
+alone_phase <- pheno_24_alone %>% pivot_wider(names_from = Phenophase, values_from = Value)
+
+phenophase_cols <- c("B","V","FL","FR")
+alone_phase[phenophase_cols] <- lapply(alone_phase[phenophase_cols], function(x) sapply(x, `[`, 1))
+str(alone_phase)
+
+value_prop <- alone_phase %>%
+  group_by(PARK, Date, SPECIES) %>%
+  summarise(
+    B_total = sum(B, na.rm = TRUE),
+    V_total = sum(V, na.rm = TRUE),
+    FL_total = sum(FL, na.rm = TRUE),
+    FR_total = sum(FR, na.rm = TRUE)
+  ) %>%
+  mutate(
+    total_all = B_total + V_total + FL_total + FR_total,
+    B_prop = B_total / total_all,
+    V_prop = V_total / total_all,
+    FL_prop = FL_total / total_all,
+    FR_prop = FR_total / total_all
+  )
+
+value_prop <- value_prop %>% 
+  pivot_longer(cols = c(FR_prop, B_prop, FL_prop, V_prop),
+               names_to = "Phenophase",
+               values_to = "Value")
+
+ggplot(alone_phase, aes(x = Date, y = FL)) + geom_point() + geom_smooth() +
+  facet_wrap(~SPECIES)
+
+phase_colors <- c(
+  "B_prop"  = "#0AA5FF",  
+  "V_prop"  = "#0FBD77",  
+  "FR_prop" = "#E0CA3C",  
+  "FL_prop" = "#D683D8"  
+)
+
+#CLAPUR 
+clapur_prop <- value_prop %>% filter(SPECIES == 'CLAPUR')
+
+ggplot(clapur_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'CLAPUR Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#COLLOM
+collom_prop <- value_prop %>% filter(SPECIES == 'COLLOM')
+
+ggplot(collom_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'COLLOM Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#COLLIN
+collin_prop <- value_prop %>% filter(SPECIES == 'COLLIN') %>% 
+  na.omit()
+
+ggplot(collin_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'COLLIN Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#GILCAP
+gilcap_prop <- value_prop %>% filter(SPECIES == 'GILCAP')
+
+ggplot(gilcap_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'GILCAP Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#PLECON
+plecon_prop <- value_prop %>% filter(SPECIES == 'PLECON') %>% 
+  na.omit()
+
+ggplot(plecon_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'PLECON Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#NAVSQU
+navsqu_prop <- value_prop %>% filter(SPECIES == 'NAVSQU')
+
+ggplot(navsqu_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'NAVSQU Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#PLAFIG
+plafig_prop <- value_prop %>% filter(SPECIES == 'PLAFIG') %>% na.omit()
+
+ggplot(plafig_prop, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'NAVSQU Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+# diverse
+  
+diverse_phase <- pheno_24_diverse 
+
+phase_colors <- c(
+  "B"  = "#0AA5FF",  
+  "V"  = "#0FBD77",  
+  "FR" = "#E0CA3C",  
+  "FL" = "#D683D8"  
+)
+
+#CLAPUR 
+clapur_phase <- diverse_phase %>% filter(SPECIES == 'CLAPUR')
+
+ggplot(clapur_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'CLAPUR Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#COLLOM
+collom_phase <- diverse_phase %>% filter(SPECIES == 'COLLOM')
+
+ggplot(collom_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'COLLOM Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#COLLIN
+collin_phase <- diverse_phase %>% filter(SPECIES == 'COLLIN') %>% 
+  na.omit()
+
+ggplot(collin_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'COLLIN Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#GILCAP
+gilcap_phase <- diverse_phase %>% filter(SPECIES == 'GILCAP')
+
+ggplot(gilcap_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'GILCAP Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#PLECON
+plecon_phase <- diverse_phase %>% filter(SPECIES == 'PLECON') %>% 
+  na.omit()
+
+ggplot(plecon_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'PLECON Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal()+ 
+  scale_color_manual(values = phase_colors)
+
+#NAVSQU
+navsqu_phase <- diverse_phase %>% filter(SPECIES == 'NAVSQU')
+
+ggplot(navsqu_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'NAVSQU Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+#PLAFIG
+plafig_phase <- diverse_phase %>% filter(SPECIES == 'PLAFIG') %>% na.omit()
+
+ggplot(plafig_phase, aes(x = Date, y = Value, color = Phenophase)) + geom_point() + geom_smooth(alpha = 0.2) +
+  facet_wrap(~PARK) + labs(title = 'NAVSQU Diverse Phenophases over Time',
+                           y = 'Proportion in Phase') + theme_minimal() + 
+  scale_color_manual(values = phase_colors)
+
+
--- Data/2024/24_pheno_wrangle.R
+++ Data/2024/24_pheno_wrangle.R
@@ -26,53 +26,257 @@ pheno_24_diverse <- pheno_24 %>% filter(PLOT == 'DIVERSE')
 dim(pheno_24_alone)
 dim(pheno_24_diverse)
 
-# GILCAP, EPIDEN, NAVSQU
+### onset
 
-# diverse 
+# diverse flowering
 new_rows_diverse <- expand_grid(
   PARK = c("BR", "RF", "SEM", "WIR"),
   PLOT = "ALONE",                
   SPECIES = c("GILCAP", "EPIDEN", "NAVSQU"),
-  Date = as.Date("2025-07-29"),
+  Date = as.Date("2024-07-25"),
   Phenophase = c("B", "FL", "FR", "V"),
   doy = yday(Date)) %>%
-  mutate(Value = if_else(Phenophase == "FL", 1, 0))
+  mutate(Value = if_else(Phenophase %in% c("FL", "FR"), 1, 0))
+
 
 pheno_24_diverse <- bind_rows(pheno_24_diverse, new_rows_diverse)
 dim(pheno_24_diverse)
 
+collom_onset <- pheno_24_diverse %>%
+  pivot_wider(names_from = Phenophase, values_from = Value) %>%
+  filter(
+    SPECIES == "COLLOM",
+    FR == 1,
+    B == 1,
+    FL %in% c(0, 1),
+  ) %>% 
+  pivot_longer(
+    cols = c(FR, B, FL, V),
+    names_to = "Phenophase",
+    values_to = "Value"
+  ) %>% 
+  mutate(onset =yday(Date)) %>% 
+  distinct(PARK, Date, .keep_all = TRUE) %>% 
+  group_by(PARK) %>% 
+  summarise(SPECIES = 'COLLOM', 
+            onset = min(doy)) %>% 
+  filter(!PARK == 'RF')
+
+
 diverse_onset_24 <- pheno_24_diverse %>% 
   filter(Phenophase == 'FL', Value != 0) %>% 
   group_by(SPECIES, PARK) %>% 
-  summarise(onset = min(doy))
+  summarise(onset = min(doy)) 
+
+diverse_onset_24 <- bind_rows(diverse_onset_24, collom_onset)
+
+# diverse fruiting onset
+
+diverse_onset_24_fr <- pheno_24_diverse %>% 
+  filter(Phenophase == 'FR', Value != 0) %>% 
+  group_by(SPECIES, PARK) %>% 
+  summarise(onset = min(doy)) 
+
 
 # alone 
 new_rows_alone <- expand_grid(
   PARK = c("BR", "RF", "SEM", "WIR"),
   PLOT = "ALONE",                
   SPECIES = c("GILCAP", "EPIDEN", "NAVSQU"),
-  Date = as.Date("2025-07-29"),
+  Date = as.Date("2024-07-29"),
   Phenophase = c("B", "FL", "FR", "V"),
   doy = yday(Date)) %>%
-  mutate(Value = if_else(Phenophase == "FL", 1, 0))
+  mutate(Value = if_else(Phenophase %in% c("FL", "FR"), 1, 0))
 
 pheno_24_alone <- bind_rows(pheno_24_alone, new_rows_alone)
 dim(pheno_24_alone)
 
+collom_onset_alone <- pheno_24_alone %>%
+  group_by(PARK, PLOT, SPECIES, Date, doy, Phenophase) %>%
+  summarise(Value = max(Value), .groups = "drop") %>%  # pick max value for duplicates
+  pivot_wider(names_from = Phenophase, values_from = Value) %>%
+  filter(
+    SPECIES == "COLLOM",
+    FR != 0,
+    B != 0
+  ) %>% 
+  pivot_longer(
+    cols = c(FR, B, FL, V),
+    names_to = "Phenophase",
+    values_to = "Value"
+  ) %>% 
+  mutate(onset = yday(Date)) %>% 
+  distinct(PARK, Date, .keep_all = TRUE) %>% 
+  filter(!(PARK %in% c('RF', 'WIR'))) %>% 
+  group_by(PARK) %>% 
+  summarise(
+    SPECIES = 'COLLOM',
+    onset = min(onset),
+    .groups = 'drop'
+  )
 
 alone_onset_24 <- pheno_24_alone %>% 
   filter(Phenophase == 'FL', Value != 0) %>% 
   group_by(SPECIES, PARK) %>% 
   summarise(onset = min(doy)) %>% arrange(onset)
 
+
+alone_onset_24 <- bind_rows(alone_onset_24, collom_onset_alone)
+
+
+# saveRDS(pheno_24_alone, "pheno_24_alone.rds")
+# saveRDS(pheno_24_diverse, "pheno_24_diverse.rds")
+# 
+# saveRDS(alone_onset_24, "alone_onset_24.rds")
+# saveRDS(diverse_onset_24, "diverse_onset_24.rds")
+
+### offset
+
+#diverse flowering 
+diverse_offset_24 <- pheno_24_diverse %>% 
+  filter(Phenophase == 'FL', Value != 0) %>% 
+  group_by(SPECIES, PARK) %>% 
+  summarise(offset = max(doy)) %>% arrange(offset)
+
+
+collom_offset_diverse <- pheno_24_diverse %>%
+  pivot_wider(names_from = Phenophase, values_from = Value) %>%
+  filter(
+    SPECIES == "COLLOM",
+    FR == 1,
+    B == 1,
+    FL %in% c(0, 1),
+  ) %>% 
+  pivot_longer(
+    cols = c(FR, B, FL, V),
+    names_to = "Phenophase",
+    values_to = "Value"
+  ) %>% 
+  mutate(offset =yday(Date)) %>% 
+  distinct(PARK, Date, .keep_all = TRUE) %>% 
+  group_by(PARK) %>% 
+  summarise(SPECIES = 'COLLOM', 
+            offset = max(doy)) %>% 
+  filter(!PARK == 'RF')
+
+diverse_offset_24 <-bind_rows(diverse_offset_24, collom_offset_diverse)
+
+diverse_offset_24 <- pheno_24_diverse %>% 
+  filter(Phenophase == 'FL', Value != 0) %>% 
+  group_by(SPECIES, PARK) %>% 
+  summarise(offset = max(doy)) %>% arrange(offset)
+
+
+collom_offset_diverse <- pheno_24_diverse %>%
+  pivot_wider(names_from = Phenophase, values_from = Value) %>%
+  filter(
+    SPECIES == "COLLOM",
+    FR == 1,
+    B == 1,
+    FL %in% c(0, 1),
+  ) %>% 
+  pivot_longer(
+    cols = c(FR, B, FL, V),
+    names_to = "Phenophase",
+    values_to = "Value"
+  ) %>% 
+  mutate(offset =yday(Date)) %>% 
+  distinct(PARK, Date, .keep_all = TRUE) %>% 
+  group_by(PARK) %>% 
+  summarise(SPECIES = 'COLLOM', 
+            offset = max(doy)) %>% 
+  filter(!PARK == 'RF')
+
+diverse_offset_24 <-bind_rows(diverse_offset_24, collom_offset_diverse)
+
+# diverse fruiting offset
+
+diverse_offset_24_fr <- pheno_24_diverse %>% 
+  filter(Phenophase == 'FR', Value != 0) %>% 
+  group_by(SPECIES, PARK) %>% 
+  summarise(offset = max(doy)) %>% arrange(offset)
+
+diverse_offset_24 <-bind_rows(diverse_offset_24, collom_offset_diverse)
+
+# combine with onset - duration
+
+diverse_fruiting_24 <- left_join(diverse_onset_24_fr, diverse_offset_24_fr)
+diverse_fruiting_24 <- rename(diverse_fruiting_24, onset_fr = onset)
+diverse_fruiting_24 <- rename(diverse_fruiting_24, offset_fr = offset)
+
+diverse_flowering_24 <- left_join(diverse_onset_24, diverse_offset_24)
+diverse_flowering_24 <- rename(diverse_flowering_24, onset_fl = onset)
+diverse_flowering_24 <- rename(diverse_flowering_24, offset_fl = offset)
+
+diverse_pheno_sum_24 <- merge(diverse_flowering_24, diverse_fruiting_24, by = c('SPECIES', 'PARK'))
+diverse_pheno_sum_24 <- diverse_pheno_sum_24 %>% arrange(PARK)
+
+diverse_pheno_sum_24 <- diverse_pheno_sum_24 %>%
+  group_by(PARK, SPECIES) %>% 
+  mutate(duration_fl = offset_fl - onset_fl,
+         duration_fr = offset_fr - onset_fr,
+         overlap = abs(duration_fr - duration_fl))
+
+ggplot(diverse_pheno_sum_24) +
+  geom_segment(aes(
+    x = onset_fl, xend = offset_fl,
+    y = SPECIES, yend = SPECIES,
+    color = "FL"
+  ), size = 3) +
+  geom_segment(aes(
+    x = onset_fr, xend = offset_fr,
+    y = SPECIES, yend = SPECIES,
+    color = "FR"
+  ), size = 3, alpha = 0.6) +
+  scale_color_manual(values = c("FL" = "#17BEBB", "FR" = "#F5BB00")) +
+  facet_wrap(~PARK) +
+  labs(
+    x = "Day of Year",
+    y = "Species",
+    color = "Phenophase",
+    title = "Flowering & Fruiting Periods"
+  ) +
+  theme_minimal()
+2
+ggplot(diverse_pheno_sum_24) +
+  geom_segment(aes(
+    x = onset_fl, xend = offset_fl,
+    y = PARK, yend = PARK,
+    color = "FL"
+  ), size = 3) +
+  geom_segment(aes(
+    x = onset_fr, xend = offset_fr,
+    y = PARK, yend = PARK,
+    color = "FR"
+  ), size = 3, alpha = 0.6) +
+  scale_color_manual(values = c("FL" = "#17BEBB", "FR" = "#F5BB00")) +
+  facet_wrap(~SPECIES) +
+  labs(
+    x = "Day of Year",
+    y = "Species",
+    color = "Phenophase",
+    title = "Flowering & Fruiting Periods"
+  ) +
+  theme_minimal()
+
 park_colors <- c(
-  "BR"  = "#955F8E",  # red
-  "RF"  = "#0F9554",  # green
-  "WIR" = "#17BEBB",  # blue
-  "SEM" = "#F5BB00"   # yellow
+  "BR"  = "#955F8E",  
+  "RF"  = "#0F9554", 
+  "WIR" = "#17BEBB",  
+  "SEM" = "#F5BB00"   
 )
 
+# alone
 ggplot(alone_onset_24, aes(x = onset, y = SPECIES, fill = PARK)) + geom_col(position = 'dodge') +
   theme_minimal() + scale_fill_manual(values = park_colors)
+
+# diverse 
+ggplot(diverse_onset_24, aes(x = onset, y = SPECIES, fill = PARK)) + geom_col(position = 'dodge') +
+  theme_minimal() + scale_fill_manual(values = park_colors) + labs(title = 'diverse')
+diverse_offset_24 <- diverse_offset_24 %>%
+  mutate(Date = as.Date(offset - 1, origin = as.Date("2024-01-01")))
+
+
+
 
 
